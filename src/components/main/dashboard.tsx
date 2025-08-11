e6ff379a80481ef80b3cfd6276456e98659c22a4
Yisus <jmercua322@g.educaand.es>
2025-08-12 00:51:32 +0200

    feat: Protec route for not login users

--- src/components/main/dashboard.tsx
+++ src/components/main/dashboard.tsx
@@ -1,8 +1,19 @@
-export default function Dashboard() {
-  return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome to the dashboard!</p>
-    </div>
-  );
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+
+    if (!user) {
+        return redirect("/api/auth/login");
+    }
+
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <p>Welcome to the dashboard!</p>
+        </div>
+    );
 }
