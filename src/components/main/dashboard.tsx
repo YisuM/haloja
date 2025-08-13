71d70d55ae512353dc5afdcce0950c7fe4a30cf6
Yisus <jmercua322@g.educaand.es>
2025-08-13 19:46:51 +0200

    refactor ui for Order

--- src/components/main/dashboard.tsx
+++ src/components/main/dashboard.tsx
@@ -1,12 +1,13 @@
 import { getUser } from "@/dal/user/get-user";
-import { OrderForm } from "./OrderForm";
+import  Order  from "@/components/main/Order";
+
 export default async function Dashboard() {
 
     await getUser();
 
     return (
-        <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 py-12 flex justify-center items-center">
-            <OrderForm />
-        </div>
+        
+        <Order />
+        
     );
 }
