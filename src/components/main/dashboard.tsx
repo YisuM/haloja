c77343a76571ba5a4122d55d60ccf1fc167607e6
Yisus <jmercua322@g.educaand.es>
2025-08-13 18:14:55 +0200

    Added OrderForm component

--- src/components/main/dashboard.tsx
+++ src/components/main/dashboard.tsx
@@ -1,12 +1,12 @@
 import { getUser } from "@/dal/user/get-user";
+import { OrderForm } from "./OrderForm";
 export default async function Dashboard() {
 
     await getUser();
 
     return (
-        <div>
-            <h1>Dashboard</h1>
-            <p>Welcome to the dashboard!</p>
+        <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 py-12 flex justify-center items-center">
+            <OrderForm />
         </div>
     );
 }
