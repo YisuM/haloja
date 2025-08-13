188ff95066a370fbbb605e8dbeef4881f68a14e9
Yisus <jmercua322@g.educaand.es>
2025-08-13 19:48:49 +0200

    feat: Order component with the orderForm built-in component

---
+++ src/components/main/Order.tsx
@@ -0,0 +1,20 @@
+import { OrderForm } from "@/components/main/OrderForm";
+import { Card,
+    CardHeader,
+    CardTitle,
+    CardContent,
+} from "../ui/card";
+
+export default function Order() {
+
+    return (
+        <Card className="max-w-2xl mx-auto mt-10">
+            <CardHeader>
+                <CardTitle className="text-2xl font-bold">Order Form</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <OrderForm />
+            </CardContent>
+        </Card>
+    );
+}
