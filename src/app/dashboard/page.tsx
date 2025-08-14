b6eea57e800e3d9c639e48427a83438e937c72fe
Yisus <jmercua322@g.educaand.es>
2025-08-14 03:22:38 +0200

    Refactor: No need ToastContainer in the dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,10 @@
-import { ToastContainer } from "react-toastify"
+/* import { ToastContainer } from "react-toastify" */
 import Dashboard from '@/components/main/dashboard';
 
 export default function Page() {
   return (
     <>
-      <ToastContainer position="bottom-right" autoClose={5000} hideProgressBar={false} closeOnClick pauseOnHover draggable pauseOnFocusLoss />
+      {/* <ToastContainer position="bottom-right" autoClose={5000} hideProgressBar={false} closeOnClick pauseOnHover draggable pauseOnFocusLoss /> */}
       <Dashboard />      
     </>
   );
