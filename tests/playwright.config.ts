8600d38db4f2fed7ad7ef2996a44995b6fa83e88
Yisus <jmercua322@g.educaand.es>
2025-08-15 17:32:49 +0200

    Some config changes; more timeout,...

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+  timeout: 90000, // 90 segundos por test
+  expect: {
+      timeout: 30000, // 60 segundos para expect()
+  },
   use: {
+    actionTimeout: 30000,
+    navigationTimeout: 30000,
     headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
